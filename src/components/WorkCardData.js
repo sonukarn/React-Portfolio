db91c5f4e71f0a0ec5c6476a37338a123d25a8de
sonukarn <s.sonukarn.1997@gmail.com>
2024-09-21 18:40:20 +0530

    added project

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -2,7 +2,16 @@ import pro1 from "../assets/javascriptdesktop.png";
 import pro2 from "../assets/digitalAgency.png";
 import pro3 from "../assets/blogpic1.png";
 import cloneproject from "../assets/clone.png";
+import kbcQuij from "../assets/kbc.png";
 const projectCardData = [
+  {
+    imgsrc: pro3,
+    title: "Bloging Website in Mern Stack",
+    text: "A fully responsive blog website designed to deliver an optimal reading experience across all devices. This project features a modern, clean layout with easy navigation, an engaging user interface Built using React, Node, Express, and Mongo Db , the site includes a dynamic blog post section, a Filter Section, and an interactive commenting system.,",
+    view: "//sonuwebtech-blog.onrender.com/",
+    technology: ["React", "Node", "Express", "MongoDb"],
+    source: "//github.com/sonukarn/Responsive-Blog-Website",
+  },
   {
     imgsrc: pro1,
     title: "CRUD Application with Pagination and Search Functionality",
@@ -12,14 +21,6 @@ const projectCardData = [
     source: "//github.com/sonukarn/HTMLCSSJAVASCRIPT_PROJECT",
   },
 
-  {
-    imgsrc: pro3,
-    title: "Bloging Website in Mern Stack",
-    text: "A fully responsive blog website designed to deliver an optimal reading experience across all devices. This project features a modern, clean layout with easy navigation, an engaging user interface Built using React, Node, Express, and Mongo Db , the site includes a dynamic blog post section, a Filter Section, and an interactive commenting system.,",
-    view: "//sonuwebtech-blog.onrender.com/",
-    technology: ["React", "Node", "Express", "MongoDb"],
-    source: "//github.com/sonukarn/Responsive-Blog-Website",
-  },
   {
     imgsrc: pro2,
     title: "Figma Template Responsive website",
@@ -35,6 +36,14 @@ const projectCardData = [
     view: "//cloneprojectweb.netlify.app/",
     technology: ["JavaScript", "React", "TailwindCss"],
     source: "//github.com/sonukarn/Figma-Responsive-Website",
+  },
+  {
+    imgsrc: kbcQuij,
+    title: "Who Wants to Be a Quizmaster",
+    text: "Who Wants to Be a Quizmaster is an interactive quiz app inspired by the popular KBC-style format. Players answer multiple-choice questions across various difficulty levels",
+    view: "//kbcclonequij.onrender.com",
+    technology: ["JavaScript", "React", "CSS"],
+    source: "//https://github.com/sonukarn/KbcCloneQuij",
   },
 ];
 export default projectCardData;
